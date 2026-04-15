097a75aeda02f10acf89b79266b83065374d97a2
Jim Yber <jimmyhaha_1999@yahoo.com>
2026-04-15 12:30:00 -0400

    Voice command chatbot

--- server.py
+++ server.py
@@ -1,12 +1,17 @@
 import base64
 import json
-from flask import Flask, render_template, request
-from worker import speech_to_text, text_to_speech, openai_process_message
-from flask_cors import CORS
 import os
+from flask import Flask, render_template, request
+from flask_cors import CORS
+
+from worker import (
+    speech_to_text as stt,
+    text_to_speech,
+    openai_process_message
+)
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 @app.route('/', methods=['GET'])
@@ -16,16 +21,47 @@ def index():
 
 @app.route('/speech-to-text', methods=['POST'])
 def speech_to_text_route():
-    return None
+    print("processing speech-to-text")
+
+    audio_binary = request.data
+    text = stt(audio_binary)
+
+    response = app.response_class(
+        response=json.dumps({'text': text}),
+        status=200,
+        mimetype='application/json'
+    )
+
+    return response
 
 
 @app.route('/process-message', methods=['POST'])
 def process_prompt_route():
+    user_message = request.json['userMessage']
+    voice = request.json['voice']
+
+    print('user_message', user_message)
+    print('voice', voice)
+
+    openai_response_text = openai_process_message(user_message)
+
+    openai_response_text = os.linesep.join(
+        [s for s in openai_response_text.splitlines() if s]
+    )
+
+    openai_response_speech = text_to_speech(openai_response_text, voice)
+
+    openai_response_speech = base64.b64encode(openai_response_speech).decode('utf-8')
+
     response = app.response_class(
-        response=json.dumps({"openaiResponseText": None, "openaiResponseSpeech": None}),
+        response=json.dumps({
+            "openaiResponseText": openai_response_text,
+            "openaiResponseSpeech": openai_response_speech
+        }),
         status=200,
         mimetype='application/json'
     )
+
     return response
 
 
--- worker.py
+++ worker.py
@@ -5,12 +5,45 @@ openai_client = OpenAI()
 
 
 def speech_to_text(audio_binary):
-    return None
+    base_url = '...'
+    api_url = base_url + '/speech-to-text/api/v1/recognize'
 
+    params = {'model': 'en-US_Multimedia'}
+
+    stt_response = requests.post(api_url, params=params, data=audio_binary).json()
+
+    if stt_response.get('results'):
+        return stt_response['results'][0]['alternatives'][0]['transcript']
+
+    return None
 
 def text_to_speech(text, voice=""):
-    return None
+    base_url = '...'
+    api_url = base_url + '/text-to-speech/api/v1/synthesize?output=output_text.wav'
+
+    if voice and voice != "default":
+        api_url += "&voice=" + voice
+
+    headers = {
+        'Accept': 'audio/wav',
+        'Content-Type': 'application/json',
+    }
+
+    return requests.post(api_url, headers=headers, json={'text': text}).content
 
 
 def openai_process_message(user_message):
-    return None
+    prompt = (
+        "Act like a personal assistant. Keep responses concise (2–3 sentences)."
+    )
+
+    openai_response = openai_client.chat.completions.create(
+        model="gpt-5-nano",
+        messages=[
+            {"role": "system", "content": prompt},
+            {"role": "user", "content": user_message}
+        ],
+        max_completion_tokens=1000
+    )
+
+    return openai_response.choices[0].message.content
